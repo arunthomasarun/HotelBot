adbdf35b5aacd42b28ba3b4809b89f7d1e53b464
arunthomasarun <arunthomas04@gmail.com>
2018-06-12 05:57:04 +0530

    Add project files.

---
+++ HotelBot/Dialogs/GreetingsDialog.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Bot.Builder.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+using Microsoft.Bot.Connector;
+
+namespace HotelBot.Dialogs
+{
+  [Serializable]
+  public class GreetingsDialog : IDialog
+  {
+    public async Task StartAsync(IDialogContext context)
+    {
+      await context.PostAsync("Hi, This is a bot developed by Arun");
+      context.Wait(MessageReceivedAsync);
+    }
+
+    private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<IMessageActivity> result)
+    {
+      var message = await result;
+      var userName = string.Empty;
+      var getName = false;
+
+      context.UserData.TryGetValue<Boolean>("GetName", out getName); 
+      context.UserData.TryGetValue<string>("Name", out userName);
+
+      if (getName)
+      {
+        userName = message.Text;
+        context.UserData.SetValue("Name", userName);
+        context.UserData.SetValue("GetName", false);
+      }
+
+      if (string.IsNullOrEmpty(userName))
+      {
+        await context.PostAsync("What is your name?");
+        context.UserData.SetValue<Boolean>("GetName", true);
+      }
+      else
+      {
+        await context.PostAsync(string.Format("Hi {0}, how can I help you today?", userName));
+      }
+      context.Wait(MessageReceivedAsync);
+    }
+  }
+}
---
+++ HotelBot/Model/RoomReservation.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Bot.Builder.FormFlow;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace HotelBot.Model
+{
+  public enum BedOptions
+  {
+    King,
+    Queen,
+    Single
+  }
+
+  public enum ExtraAmneties
+  {
+    SwimmingPool,
+    Gym,
+    Library,
+    WiFi,
+    Laundry
+  }
+
+  [Serializable]
+  public class RoomReservation
+  {
+
+    public BedOptions? BedSizeOptions;
+    public int? NoOfOccupants;
+    public int? NoOfDaysToStay;
+    public DateTime? CheckInDate;
+    public List<ExtraAmneties> Amneties;
+    public static IForm<RoomReservation> BuildForm()
+    {
+      return new FormBuilder<RoomReservation>()
+        .Message("Welcome to the booking section!!!")
+        .Build();
+    }
+  }
+}
